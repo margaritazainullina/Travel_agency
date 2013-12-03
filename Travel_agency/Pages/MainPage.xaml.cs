d4da6da961ee24eb7985bc5f9019ea90b3eb87f2
Zainulllina.Margarita <margarita.zainullina@gmail.com>
2013-12-03 18:48:28 +0200

    asdf

---
+++ Travel_agency/Pages/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Travel_agency
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        DataSet ds;             //Набор данных
+        OleDbDataAdapter da;
+
+        private void EnterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (EnterBtn.Content.ToString() == "Вход")
+            {
+                try
+                {
+                    string connString = myPublic.connString;
+                    string selectCmd = "SELECT name FROM Employee WHERE (((login)='" + Login.Text + "') AND ((password)='" + Pasw.Text + "'))";
+                    da = new OleDbDataAdapter(selectCmd, connString);
+                    ds = new DataSet();
+                    da.Fill(ds, "Employee");
+                    lbl2.Visibility = Visibility.Collapsed;
+                    Pasw.Visibility = Visibility.Collapsed;
+                    Login.Visibility = Visibility.Collapsed;
+
+                    EnterBtn.Content = "Выход";
+                    Lb1.DataContext = ds;
+                    connString = myPublic.connString;
+                    DataRowView das = (DataRowView)Lb1.Items[0];
+                }
+                catch
+                {
+                    lbl2.Visibility = Visibility.Visible;
+                    Pasw.Visibility = Visibility.Visible;
+                    Pasw.Text = "";
+                    Login.Text = "";
+                    Login.Visibility = Visibility.Visible;
+                    EnterBtn.Content = "Вход";
+                    MessageBox.Show("Не удалось авторизироваться!");
+                }
+            }
+            else
+            {
+                DataRowView das = (DataRowView)Lb1.Items[0];
+                lbl1.Content = "Вы успешно авторизовались";
+                lbl2.Visibility = Visibility.Visible;
+                Pasw.Visibility = Visibility.Visible;
+                Pasw.Text = "";
+                Login.Text = "";
+                Login.Visibility = Visibility.Visible;
+                EnterBtn.Content = "Вход";
+                string connString = myPublic.connString;
+                
+            }
+             
+               
+        }
+    }
+}
---
+++ Travel_agency/myPublic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//Добавляем для работы с ADO
+using System.Data;
+using System.Data.OleDb;
+
+
+namespace Travel_agency
+{
+    class myPublic
+    {
+        //Строка соединения
+        public static string connString =
+            @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\travel_agency.accdb";
+              
+    }
+}
